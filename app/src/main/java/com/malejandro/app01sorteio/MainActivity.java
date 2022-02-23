b4492059569b58ed1b66216478f682eed71e7881
Matheus Alejandro <matheus.alejandro@oracle.com>
2022-02-23 16:40:34 -0300

    App sorteio basico

--- app/src/main/java/com/malejandro/app01sorteio/MainActivity.java
+++ app/src/main/java/com/malejandro/app01sorteio/MainActivity.java
@@ -3,12 +3,28 @@ package com.malejandro.app01sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    //declarar todos os campos alteraveis antes
+    private TextView textoDoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //apontar para os campos
+        textoDoResultado = findViewById(R.id.textResultado);
     }
+
+    //fazer os processamentos abaixo
+    public void cliqueButtonRealizarSorteio(View view) {
+        int numeroSorteado = new Random().nextInt(11);
+
+        textoDoResultado.setText("O número selecionado: " + numeroSorteado);
+    }
+
 }
